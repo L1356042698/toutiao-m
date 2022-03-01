a406748588b3bd78643334a28204a4a3e767ae80
刘品洲 <1356042698@qq.com>
2022-03-01 14:59:02 +0800

    项目初始化目录结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//加载全局样式
+import './styles/index.css'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+//vue脚手架项目 - 默认的配置文件名
+//webpack+node环境 - 到处配置对象
+module.exports = {
+    devServer: {
+        port: 4000,
+        open: true //流浪器自动打开
+    },
+    lintOnSave: false //关闭eslint检查
+}
